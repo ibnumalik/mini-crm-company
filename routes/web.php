419e4864a3e59498d7895ae80788313ad93bb730
Shaiful <ibnumalik@tbd.my>
2019-10-24 22:45:53 +0800

    Add resource route for company and employee

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('companies', 'CompanyController');
+Route::resource('employees', 'EmployeeController');
