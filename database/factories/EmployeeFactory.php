3e8b10483c64882626e3b2eaab1ea7eec88c6d9d
Shaiful <ibnumalik@tbd.my>
2019-10-24 12:38:43 +0800

    Add employee model, controller and migration files

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Employee;
+use Faker\Generator as Faker;
+
+$factory->define(Employee::class, function (Faker $faker) {
+    return [
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'email' => $faker->companyEmail,
+        'phone' => $faker->phoneNumber,
+        'company_id' => \App\Company::all()->random()->id
+    ];
+});
---
+++ database/migrations/2019_10_24_011232_create_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('company_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->foreign('company_id')->references('id')->on('companies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
