214e8262a10e77ed160e54d1355c09ee2c13ceb5
Shaiful <ibnumalik@tbd.my>
2019-10-26 16:31:13 +0800

    Add view to show list of employees

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -14,7 +14,9 @@ class EmployeeController extends Controller
      */
     public function index()
     {
-        //
+        $employees = Employee::all();
+
+        return view('employees.index', compact('employees'));
     }
 
     /**
---
+++ resources/views/employees/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="container">
+        <div class="row mb-3">
+            <div class="col-sm">
+                <a href="{{ route('employees.create') }}" class="btn btn-primary">
+                    Create new employee
+                </a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm">
+                <h1>Employee List</h1>
+                <ol>
+                    @foreach ($employees as $employee)
+                    <li>
+                        <a href="{{ route('employees.show', ['employee'=> $employee->id]) }}">
+                            {{$employee->first_name . ' ' . $employee->last_name}}
+                        </a>
+                    </li>
+                    @endforeach
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
