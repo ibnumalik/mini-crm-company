98d6b8e54119234cf8241fc060d601b344f04a84
Shaiful <ibnumalik@tbd.my>
2019-10-26 20:01:13 +0800

    Add menu item company and employee in dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,22 +49,23 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item">
-                                <a href="{{ route('home') }}" class="nav-link">
-                                    Dashboard
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('companies.index') }}" class="nav-link">
-                                    Companies
-                                </a>
-                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+                                    <a class="dropdown-item" href="{{ route('home') }}">
+                                        Dashboard
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route('companies.index') }}">
+                                        Companies
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route('employees.index') }}">
+                                        Employees
+                                    </a>
+                                    <div class="dropdown-divider"></div>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
