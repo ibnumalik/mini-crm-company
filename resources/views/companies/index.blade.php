d5284af2ee4334917fc8cbba386f5f37675f7a00
Shaiful <ibnumalik@tbd.my>
2019-11-05 22:44:46 +0800

    Adapt employee and company index to star admin

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -1,33 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="container">
-        <div class="row mb-3">
-            <div class="col-sm">
-                <a href="{{ route('companies.create') }}" class="btn btn-primary">
-                    Create new company
-                </a>
-            </div>
-        </div>
+<div class="main-panel">
+    <div class="content-wrapper">
+        <b-container fluid>
+            <h1>Company List</h1>
+            <ol>
+                @foreach ($companies as $company)
+                <li>
+                    <a href="{{ route('companies.show', ['company'=> $company->id]) }}">
+                        {{$company->name}}
+                    </a>
+                </li>
+                @endforeach
+            </ol>
 
-        <div class="row">
-            <div class="col-sm">
-                <h1>Company List</h1>
-                <ol>
-                    @foreach ($companies as $company)
-                    <li>
-                        <a href="{{ route('companies.show', ['company'=> $company->id]) }}">
-                            {{$company->name}}
-                        </a>
-                    </li>
-                    @endforeach
-                </ol>
-            </div>
-        </div>
-
-        {{ $companies->links() }}
-
+            {{ $companies->links() }}
+        </b-container>
     </div>
 </div>
 
--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,33 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="container">
-        <div class="row mb-3">
-            <div class="col-sm">
-                <a href="{{ route('employees.create') }}" class="btn btn-primary">
-                    Create new employee
-                </a>
-            </div>
-        </div>
 
-        <div class="row">
-            <div class="col-sm">
-                <h1>Employee List</h1>
-                <ol>
-                    @foreach ($employees as $employee)
-                    <li>
-                        <a href="{{ route('employees.show', ['employee'=> $employee->id]) }}">
-                            {{$employee->first_name . ' ' . $employee->last_name}}
-                        </a>
-                    </li>
-                    @endforeach
-                </ol>
-            </div>
-        </div>
+<div class="main-panel">
+    <div class="content-wrapper">
+        <b-container fluid>
 
-        {{ $employees->links() }}
+            <h1>Employee List</h1>
+            <ol>
+                @foreach ($employees as $employee)
+                <li>
+                    <a href="{{ route('employees.show', ['employee'=> $employee->id]) }}">
+                        {{$employee->first_name . ' ' . $employee->last_name}}
+                    </a>
+                </li>
+                @endforeach
+            </ol>
+            {{ $employees->links() }}
+        </b-container>
     </div>
 </div>
-
 @endsection
