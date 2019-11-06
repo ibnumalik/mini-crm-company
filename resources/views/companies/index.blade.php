01a2c7f516465dec8f57c808c5df9b34bf489626
Shaiful <ibnumalik@tbd.my>
2019-11-06 10:44:09 +0800

    Use card to display list of employee and company

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -4,18 +4,23 @@
 <div class="main-panel">
     <div class="content-wrapper">
         <b-container fluid>
-            <h1>Company List</h1>
-            <ol>
-                @foreach ($companies as $company)
-                <li>
-                    <a href="{{ route('companies.show', ['company'=> $company->id]) }}">
-                        {{$company->name}}
-                    </a>
-                </li>
-                @endforeach
-            </ol>
+            <b-row>
+                <b-col cols="12" lg="4">
+                    <b-card title="Company List">
+                        <ol>
+                            @foreach ($companies as $company)
+                            <li>
+                                <a href="{{ route('companies.show', ['company'=> $company->id]) }}">
+                                    {{$company->name}}
+                                </a>
+                            </li>
+                            @endforeach
+                        </ol>
 
-            {{ $companies->links() }}
+                        {{ $companies->links() }}
+                    </b-card>
+                </b-col>
+            </b-row>
         </b-container>
     </div>
 </div>
--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -6,17 +6,23 @@
     <div class="content-wrapper">
         <b-container fluid>
 
-            <h1>Employee List</h1>
-            <ol>
-                @foreach ($employees as $employee)
-                <li>
-                    <a href="{{ route('employees.show', ['employee'=> $employee->id]) }}">
-                        {{$employee->first_name . ' ' . $employee->last_name}}
-                    </a>
-                </li>
-                @endforeach
-            </ol>
-            {{ $employees->links() }}
+            <b-row>
+                <b-col cols="12" lg="4">
+                    <b-card title="Employee List">
+                        <ol>
+                            @foreach ($employees as $employee)
+                            <li>
+                                <a href="{{ route('employees.show', ['employee'=> $employee->id]) }}">
+                                    {{$employee->first_name . ' ' . $employee->last_name}}
+                                </a>
+                            </li>
+                            @endforeach
+                        </ol>
+                        {{ $employees->links() }}
+                    </b-card>
+                </b-col>
+            </b-row>
+
         </b-container>
     </div>
 </div>
