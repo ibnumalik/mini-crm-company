c89d6c5984f98208feb2236855d418d8aeb3eaf5
Shaiful <ibnumalik@tbd.my>
2020-01-10 00:23:09 +0800

    Remove unused Request import in company controller

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Company;
 use App\Http\Requests\StoreCompany;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 
