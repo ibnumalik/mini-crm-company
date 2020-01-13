b0023b37b6641cc5bce7c88195b0059952618a06
Shaiful <ibnumalik@tbd.my>
2020-01-14 01:16:51 +0800

    Create Mailable class for company created email

---
+++ app/Mail/CompanyCreated.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use App\Company;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class CompanyCreated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * The company instance.
+     *
+     * @val Company
+     */
+    public $company;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Company $company)
+    {
+        $this->company = $company;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.company.created');
+    }
+}
