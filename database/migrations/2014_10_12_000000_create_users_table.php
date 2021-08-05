34f8e0d8351d397adb1303095a2c15e05422a4fe
Shaiful Aiman Malik <ibnumalikmy@gmail.com>
2021-08-05 15:50:12 +0800

    comment remember token in create users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('remember_token');
+            // $table->string('remember_token');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
