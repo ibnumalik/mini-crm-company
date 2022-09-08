4f77d9edf0043ad11f30266a21d4199caef1d0d4
Shaiful Aiman Malik <ibnumalikmy@gmail.com>
2022-09-08 14:39:02 +0800

    add RouterLink component

---
+++ resources/js/components/RouterLink.vue
@@ -0,0 +1,14 @@
+<template>
+  <Link :href="to">
+    <slot />
+  </Link>
+</template>
+
+<script>
+import { Link } from '@inertiajs/inertia-vue';
+
+export default {
+  components: { Link },
+  props: ['to'],
+};
+</script>
